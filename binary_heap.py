c9fc8924e249badafb94c72a955c8b6b0f3bb41c
LukasVotocek <votocekl@arcig.cz>
2025-03-21 23:03:17 +0100

    Update binary_heap.py

--- binary_heap.py
+++ binary_heap.py
@@ -8,25 +8,46 @@ class Element:
  
 
 class BinaryHeap:
-
     def __init__(self):
-        self.heap = []
-    
-    def push(self, element):
-        # naivni implementace binarni haldy
+        self.heap: list[Element] = []
+        
+    def push(self,element:Element):
         self.heap.append(element)
+        idx=(len(self.heap))-1
+        while idx>0:
+            idx_rodic = (idx-1) // 2
+            rodic = self.heap[idx_rodic] 
+            dite = element           
+            if rodic.priority > dite.priority:
+                self.heap[idx] = rodic
+                self.heap[idx_rodic] = dite
+            else: 
+                break
+         
 
     def pop(self):
-        # naivni implementace binarni haldy
-        # najdi nejmensi prvek a vrat ho
-        if not self.heap:
-            raise Exception("Heap is empty")
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap.pop(min_idx)
+        min_element=self.heap[0]
+        last_element=self.heap.pop()
+        if not self.heap: 
+            return min_element
+        else:
+            self.heap[0]=last_element
+            idx=0
 
+            while True:
+                smallest=idx
+                left_child = 2 * idx + 1
+                right_child = 2 * idx + 2
+                if left_child <= len(self.heap)-1 and self.heap[left_child].priority < self.heap[smallest].priority:
+                        smallest = left_child
+                if  right_child <= len(self.heap)-1 and self.heap[right_child].priority < self.heap[smallest].priority:
+                        smallest = right_child
+                if smallest != idx:
+                    self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]
+                    idx = smallest
+                else: 
+                    break
+            return min_element
 
     def head(self):
         if not self.heap:
